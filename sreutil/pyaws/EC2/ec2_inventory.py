7dd590e3687db3448fdcbecf8fa87dada063e061
Dakota Remenyi <Dakota.Remenyi@coalfire.com>
2025-05-30 20:27:46 -0400

    updated ec2-inventory and create-snapshots

--- sreutil/pyaws/EC2/ec2_inventory.py
+++ sreutil/pyaws/EC2/ec2_inventory.py
@@ -1,157 +1,98 @@
-# import boto3
-# import csv
-# from prettytable import PrettyTable
-
-# def ec2_inventory(args, output_file=None):
-    
-#     session = boto3.Session(
-#         profile_name=args.profile, 
-#         region_name=args.region
-                            
-#                 )
-
-#     ec2 = session.client('ec2')
-
-#     result = []
-#     response = ec2.describe_instances().get('Reservations')
-
-#     # Create a PrettyTable instance
-#     table = PrettyTable()
-#     table.field_names = ['DNS Name', 'Name', 'Subnet', 'Instance Type', 'Private IP', 'Image ID']
-
-#     for item in response:
-#         for each in item['Instances']:
-#             if "Tags" in each:
-#                 name = None
-#                 for tag in each['Tags']:
-#                     if tag['Key'] == 'Name':
-#                         name = tag['Value']
-
-#                 # Check if the keys exist before accessing them
-#                 dnsname = each['PrivateDnsName'] if 'PrivateDnsName' in each else 'N/A'
-#                 subnet = each['SubnetId'] if 'SubnetId' in each else 'N/A'
-#                 instance_type = each['InstanceType'] if 'InstanceType' in each else 'N/A'
-#                 private_ip = each['PrivateIpAddress'] if 'PrivateIpAddress' in each else 'N/A'
-#                 image_id = each['ImageId'] if 'ImageId' in each else 'N/A'
-
-#                 # Append a row to the table
-#                 table.add_row([dnsname, name, subnet, instance_type, private_ip, image_id])
-
-#     # If output file is specified, write the table to the file in CSV format
-#     if output_file:
-#         with open(output_file, 'w') as file:
-#             file.write(table.get_string())
-#         print(f'Table written to file {output_file}.')
-#     # Otherwise, print the table to the console
-#     else:
-#         print(table)
-
-
-##VERSION 2 GRABS EVERYTHING XCEPT SUBNET NAME
-
-# import boto3
-# from prettytable import PrettyTable
-# import argparse
-
-# def ec2_inventory(args):
-#     profile = args.profile
-#     region = args.region
-#     output = args.output
-
-#     session = boto3.Session(profile_name=profile, region_name=region)
-#     ec2 = session.resource('ec2')
-
-#     table = PrettyTable()
-#     table.field_names = ["Instance ID", "Instance Type", "Instance State", "Public IP", "Private IP", "Subnet ID", "Subnet Name", "VPC ID", "Name Tag"]
-
-#     for instance in ec2.instances.all():
-#         name_tag = ''
-#         subnet_name = ''
-
-#         if instance.tags:
-#             for tag in instance.tags:
-#                 if tag['Key'] == 'Name':
-#                     name_tag = tag['Value']
-#                 if tag['Key'] == 'Subnet':
-#                     subnet_name = tag['Value']
-
-#         subnet = instance.subnet
-#         vpc = instance.vpc
-
-#         table.add_row([instance.id, instance.instance_type, instance.state['Name'], instance.public_ip_address, instance.private_ip_address, subnet.id, subnet_name, vpc.id, name_tag])
-
-#     if output:
-#         with open(output, 'w') as f:
-#             f.write(table.get_string())
-#     else:
-#         print(table)
-
-# def main():
-#     parser = argparse.ArgumentParser(description='Get EC2 inventory')
-#     parser.add_argument('--profile', required=True, help='AWS profile to use')
-#     parser.add_argument('--region', default='us-gov-west-1', help='AWS region to use')
-#     parser.add_argument('--output', help='Output CSV file')
-
-#     args = parser.parse_args()
-#     ec2_inventory(args)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 import boto3
 from prettytable import PrettyTable
-import argparse
+import csv # Import the CSV module
+from botocore.exceptions import NoCredentialsError, ClientError
+from colorama import Fore, Style, init
 
 def ec2_inventory(args):
     profile = args.profile
     region = args.region
-    output = args.output
+    output_file_path = args.output
 
-    session = boto3.Session(profile_name=profile, region_name=region)
-    ec2 = session.resource('ec2')
-    ec2_client = session.client('ec2')
+    init(autoreset=True)
 
-    table = PrettyTable()
-    table.field_names = ["Instance ID", "Instance Type", "Instance State", "Public IP", "Private IP", "Subnet ID", "Subnet Name", "VPC ID", "Name Tag"]
+    print(f"{Fore.CYAN}Attempting to gather EC2 inventory for region '{region}' using profile '{profile}'...{Style.RESET_ALL}")
 
-    for instance in ec2.instances.all():
-        name_tag = ''
-        subnet_name = ''
+    try:
+        session = boto3.Session(profile_name=profile, region_name=region)
+        ec2_resource = session.resource('ec2')
+        ec2_client = session.client('ec2')
 
-        if instance.tags:
-            for tag in instance.tags:
+        print(f"{Fore.CYAN}Fetching all subnet details...{Style.RESET_ALL}")
+        all_subnets_response = ec2_client.describe_subnets()
+        subnet_name_map = {}
+        for subnet_detail in all_subnets_response.get('Subnets', []):
+            s_name = ''
+            for tag in subnet_detail.get('Tags', []):
                 if tag['Key'] == 'Name':
-                    name_tag = tag['Value']
+                    s_name = tag['Value']
+                    break
+            subnet_name_map[subnet_detail['SubnetId']] = s_name
+        print(f"{Fore.CYAN}Found {len(subnet_name_map)} subnets.{Style.RESET_ALL}")
 
-        subnet = instance.subnet
-        vpc = instance.vpc
+        # Define field names for both PrettyTable and CSV header
+        field_names = ["Instance ID", "Name Tag", "Instance State", "Instance Type", 
+                       "Public IP", "Private IP", "Subnet ID", "Subnet Name", "VPC ID"]
+        
+        table = PrettyTable()
+        table.field_names = field_names
+        table.align = "l"
 
-        # Fetch the subnet details to get the subnet name
-        if subnet:
-            subnet_details = ec2_client.describe_subnets(SubnetIds=[subnet.id])
-            for subnet_detail in subnet_details['Subnets']:
-                for tag in subnet_detail.get('Tags', []):
+        # Prepare data rows for both PrettyTable and CSV
+        data_for_output = []
+
+        print(f"{Fore.CYAN}Fetching instance details... (this might take a moment for many instances){Style.RESET_ALL}")
+        instance_count = 0
+        for instance in ec2_resource.instances.all():
+            instance_count += 1
+            name_tag = ''
+            if instance.tags:
+                for tag in instance.tags:
                     if tag['Key'] == 'Name':
-                        subnet_name = tag['Value']
+                        name_tag = tag['Value']
+                        break
 
-        table.add_row([instance.id, instance.instance_type, instance.state['Name'], instance.public_ip_address, instance.private_ip_address, subnet.id if subnet else 'N/A', subnet_name, vpc.id if vpc else 'N/A', name_tag])
+            subnet_id_str = instance.subnet_id if instance.subnet_id else 'N/A'
+            current_subnet_name = subnet_name_map.get(instance.subnet_id, '') if instance.subnet_id else ''
+            vpc_id_str = instance.vpc_id if instance.vpc_id else 'N/A'
 
-    if output:
-        with open(output, 'w') as f:
-            f.write(table.get_string())
-    else:
-        print(table)
+            row_data = [
+                instance.id,
+                name_tag,
+                instance.state['Name'] if instance.state else 'N/A',
+                instance.instance_type,
+                instance.public_ip_address if instance.public_ip_address else 'N/A',
+                instance.private_ip_address if instance.private_ip_address else 'N/A',
+                subnet_id_str,
+                current_subnet_name,
+                vpc_id_str
+            ]
+            table.add_row(row_data) # Add to PrettyTable
+            data_for_output.append(row_data) # Add to list for CSV
 
-def main():
-    parser = argparse.ArgumentParser(description='Get EC2 inventory')
-    parser.add_argument('--profile', required=True, help='AWS profile to use')
-    parser.add_argument('--region', default='us-gov-west-1', help='AWS region to use')
-    parser.add_argument('--output', help='Output CSV file')
+        print(f"{Fore.GREEN}Processed {instance_count} instances.{Style.RESET_ALL}")
 
-    args = parser.parse_args()
-    ec2_inventory(args)
+        if output_file_path:
+            try:
+                with open(output_file_path, 'w', newline='') as csvfile: # Open with newline='' for csv
+                    csv_writer = csv.writer(csvfile)
+                    csv_writer.writerow(field_names) # Write header row
+                    csv_writer.writerows(data_for_output) # Write all data rows
+                print(f"{Fore.GREEN}Inventory data successfully written to CSV file: {output_file_path}{Style.RESET_ALL}")
+            except IOError as e:
+                print(f"{Fore.RED}Error writing to CSV file {output_file_path}: {e}{Style.RESET_ALL}")
+        else:
+            print(table) # Print PrettyTable to console if no output file
 
-if __name__ == "__main__":
-    main()
+    except NoCredentialsError:
+        print(f"{Fore.RED}No AWS credentials found for profile '{profile}'. Please configure your AWS credentials.{Style.RESET_ALL}")
+    except ClientError as e:
+        error_code = e.response.get("Error", {}).get("Code")
+        error_message = e.response.get("Error", {}).get("Message", str(e))
+        if "AuthFailure" in error_code or "OptInRequired" in error_code:
+             print(f"{Fore.RED}AWS Authentication/Authorization Error for profile '{profile}' in region '{region}': {error_message}{Style.RESET_ALL}")
+        else:
+            print(f"{Fore.RED}AWS ClientError in region '{region}' (Profile: {profile}): {error_message} (Code: {error_code}){Style.RESET_ALL}")
+    except Exception as e:
+        print(f"{Fore.RED}An unexpected error occurred: {type(e).__name__} - {e}{Style.RESET_ALL}")
+
